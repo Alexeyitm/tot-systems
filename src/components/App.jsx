2e3eff9a5e6989634b289456110a61ea91e69aa3
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-12 00:04:14 +0500

    feat: create data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,14 @@ import s from'./App.module.css';
 import Header from './Header/Header'
 import Folders from './Folders/Folders'
 import Letter from './Letter/Letter'
+import data from './../data/data'
 
 function App() {
   return (
     <div className={s.app}>
-      <Header />
+      <Header 
+        user={data.user}
+      />
       <Folders />
       <Letter />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Header.module.css';
 
-function Header() {
+function Header({ user }) {
   return (
     <header className={s.header}>
       <div className={s.logo}></div>
+      <h1 className={s.name}>Пользователь: {user.name.split(' ')[0]}</h1>
     </header>
   );
 }

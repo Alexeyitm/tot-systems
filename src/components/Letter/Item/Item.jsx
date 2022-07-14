a04bba7856b752e6aba05b8cf2e03a75b2ffa9f1
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-14 20:06:41 +0500

    feat: add marker

--- src/components/Letter/Item/Item.jsx
+++ src/components/Letter/Item/Item.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Item.module.css';
 
 function Item({ item }) {
+
+  const[isMarker, setIsMarker] = useState(item.marker)
+
+  const handleClickMarker = () => {
+    item.marker = !item.marker;
+    setIsMarker(item.marker);
+  }
+
   return (
     <div className={s.item}>
-      <div className={s.buttons}></div>
+      <div className={s.buttons}>
+        <button
+          className={s.button + " " + (isMarker ? s.marker_active : s.marker)}
+          onClick={handleClickMarker}
+        ></button>
+        <button className={`${s.button} ${s.delete}`}></button>
+      </div>
       <h3 className={s.author}>
         {item.read === undefined ? "Кому: " + item.to : "От: " + item.from}
       </h3>
